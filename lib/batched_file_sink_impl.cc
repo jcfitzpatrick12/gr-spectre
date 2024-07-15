f569527a4aaf94d69fb01bc11720d6800ca8039d
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-07-15 14:27:06 +0100

    chore: temp commit

--- lib/batched_file_sink_impl.cc
+++ lib/batched_file_sink_impl.cc
@@ -135,22 +135,22 @@ void batched_file_sink_impl::write_tag_states_to_hdr(int noutput_items) {
     std::vector<tag_t> frequency_tags;
     get_tags_in_range(frequency_tags, 0, abs_start_N, abs_end_N, _frequency_key);
     
-    // // Iterate through each tag and compute the number of samples for each tag interval
-    // for (const tag_t &frequency_tag : frequency_tags) {
-    //     // Compute the number of samples then update the active tag
-    //     int32_t num_samples_active_frequency = frequency_tag.offset - _active_frequency_tag.offset;
-    //     // Compute the active frequency 
-    //     float active_frequency = pmt::to_float(_active_frequency_tag.value);
+    // Iterate through each tag and compute the number of samples for each tag interval
+    for (const tag_t &frequency_tag : frequency_tags) {
+        // Compute the number of samples then update the active tag
+        int32_t num_samples_active_frequency = frequency_tag.offset - _active_frequency_tag.offset;
+        // Compute the active frequency 
+        float active_frequency = pmt::to_float(_active_frequency_tag.value);
 
-    //     std::cout << "Active frequency: " << active_frequency << std::endl;
-    //     std::cout << "Num samples: " << num_samples_active_frequency <<std::endl;
+        std::cout << "Active frequency: " << active_frequency << std::endl;
+        std::cout << "Num samples: " << num_samples_active_frequency <<std::endl;
 
-    //     // and write to file
-    //     write_to_file(_hdr_file, &active_frequency, sizeof(float));
-    //     write_to_file(_hdr_file, &num_samples_active_frequency, sizeof(int32_t));
-    //     // Update the active frequency
-    //     _active_frequency_tag = frequency_tag;
-    // }
+        // and write to file
+        write_to_file(_hdr_file, &active_frequency, sizeof(float));
+        write_to_file(_hdr_file, &num_samples_active_frequency, sizeof(int32_t));
+        // Update the active frequency
+        _active_frequency_tag = frequency_tag;
+    }
 
     // // if _open_new_file is set to true, we need to do some clean-up before opening the next file at the next call of the work function
     // if (_open_new_file) 
