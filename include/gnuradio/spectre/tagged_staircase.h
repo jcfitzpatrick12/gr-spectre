1a0564176a49ffef7bd8885546cffecdcfd46832
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2026-01-11 16:27:56 +0000

    feat: Refactor the tagged staircase block

--- include/gnuradio/spectre/tagged_staircase.h
+++ include/gnuradio/spectre/tagged_staircase.h
@@ -15,9 +15,19 @@ namespace gr {
 namespace spectre {
 
 /*!
- * \brief <+description of block+>
- * \ingroup spectre
+ * \brief Models I/Q samples produced by a receiver whose center frequency is swept over a
+ * range of frequencies. \ingroup spectre \details Produces complex samples where the real
+ * component is incremented by a unit after some number of samples and the imaginary
+ * component is always zero. Consecutive samples with identical values form a step,
+ * modeling samples captured at the same center frequency. The first sample of each step
+ * is tagged with the corresponding center frequency. Step lengths increase by a fixed
+ * amount compared to the previous, until it would surpass a maximum length (then it's
+ * reset).
  *
+ * The motivation is to validate spectrograms produced by Spectre by enabling direct
+ * comparison with analytical results. The constant value within each step allows explicit
+ * evaluation of the DFT, while the variable step lengths reflect the behavior of a
+ * receiver periodically retuned using message passing in GNU Radio.
  */
 class SPECTRE_API tagged_staircase : virtual public gr::sync_block
 {
@@ -25,18 +35,21 @@ public:
     typedef std::shared_ptr<tagged_staircase> sptr;
 
     /*!
-     * \brief Return a shared_ptr to a new instance of spectre::tagged_staircase.
-     *
-     * To avoid accidental use of raw pointers, spectre::tagged_staircase's
-     * constructor is in a private implementation
-     * class. spectre::tagged_staircase::make is the public interface for
-     * creating new instances.
+     * \brief Make a tagged staircase.
+     * \param min_samples_per_step The length of the shortest step in the staircase
+     * \param max_samples_per_step The maximum allowed length (inclusive) of a step in the
+     * staircase
+     * \param hop_freq The amount to increment the modelled center frequency at each new
+     * step \param step_increment Each new step increases in length by this amount
+     * \param sample_rate The modelled sample rate of the output stream. According to this
+     * value, the initial modelled center frequency is set such that on performing an FFT,
+     * the edge of the spectrum will be at 0Hz
      */
     static sptr make(int min_samples_per_step = 4000,
                      int max_samples_per_step = 5000,
-                     float freq_step = 32000,
+                     float hop_freq = 32000,
                      int step_increment = 200,
-                     int samp_rate = 32000);
+                     float sample_rate = 32000);
 };
 
 } // namespace spectre
